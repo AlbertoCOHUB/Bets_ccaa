9b764406ea714a02dc90b86105bf0748de75112f
AlberCHub <albertoconcejal@gmail.com>
2026-01-17 18:33:12 +0100

    preliminar

---
+++ script_01.R
@@ -0,0 +1,12 @@
+getwd() # saber directorio de trabajo 
+setwd("C:/Users/alber/alber/Documents/BETTING") # ojo con la ruta / o \\
+
+#dia 2 manejo_datos con funciones
+library(tidyverse)
+library(dplyr)
+library(swirlify)
+
+str(apuestas)
+glimpse(apuestas)
+View(apuestas)
+summary(apuestas)
